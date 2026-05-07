1e8498b5099d3a34b5cb97efbe60ff32edcd3fb1
Andres Felipe Arredondo <andres.arredondo9608@gmail.com>
2026-05-06 23:46:49 -0500

    Initializing Project

---
+++ src/test/java/com/andres/serverless_java_products_api/ServerlessJavaProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.serverless_java_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerlessJavaProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
